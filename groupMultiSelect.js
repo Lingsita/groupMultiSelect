b93a383790d3b350382d9887312de85a9c9cfc1e
lingsita <linglung1047@gmail.com>
2017-06-16 13:30:02 -0500

    onChange method and styles

--- groupMultiSelect.js
+++ groupMultiSelect.js
@@ -1,35 +1,82 @@
-$.fn.groupMultiSelect = function(config) {
+(function($){
+    $.fn.groupMultiSelect = function(config) {
 
-  var html = "<div class='groupSelect'><div class='select'>--------</div><div class='options'>";
-  $.each(config.options, function(){
-    html += "<div class='group-select'><input type='checkbox' class='group-select-option' value='"+this.label+"'>"+this.label;
-    $.each(this.subItems, function(){
-      html += "<div class='subitem'><input type='checkbox' class='group-select-option-subitem' value='"+this+"'>"+this+"</div>";
-    })
-    html+="</div>";
-  });
-  html+= "</div></div></div>"
-  $( this ).html(html);
-  $(".select").on('click', function(){
-    if($(".options").hasClass('group-select-hide')){
-      $(".options").removeClass('group-select-hide');
-    }else{
-      $(".options").addClass('group-select-hide');
-    }
-  });
-  $(document).click(function(event) {
-    if(!$(event.target).closest('.groupSelect').length) {
-      $(".options").addClass('group-select-hide');
-    }else{
-      $(".options").removeClass('group-select-hide');
-    }
-  });
-  $(".group-select-option").on('click', function(){
-    var valor = $(this).val();
-    $('.select').html('<div class="group-selection">'+valor+'</div>')
-  });
-  this.val = function(){
-    return this.html();
-  }
-  return this;
-};
+        this.parentValue = '';
+        this.childrenValue = '';
+
+        var settings = $.extend({
+            // These are the defaults.
+            options: {},
+            placeholder: "Group Select",
+            onChange: function (parent, children) {
+            }
+        }, config );
+
+        var html = "<div class='groupSelect'><div class='select'><div class='placeholder'> "+settings.placeholder+"</div></div><div class='options group-select-hide'>";
+        $.each(settings.options, function(i){
+            var id_parent = this.label.toLowerCase().replace(/ /g,'-')
+            html += "<div><input type='radio' name='group-select' class='group-select-option' id='id_parent_"+i+"' value='"+this.label+"'><label for='id_parent_"+i+"'>"+this.label+"</label>";
+            if(this.subItems.length>0){
+                html+="<div class='subitem-group "+id_parent+"' style='display: none'>";
+                $.each(this.subItems, function(index){
+                    html += "<div class='subitem'><input type='checkbox' id='subitem_"+id_parent+"_"+index+"' class='group-select-option-subitem' data-parent='"+id_parent+"' value='"+this+"'><label for='subitem_"+id_parent+"_"+index+"'>"+this+"</label></div>";
+                })
+                html+="</div>";
+            }
+            html+="</div>";
+        });
+        html+= "</div></div></div>"
+        $( this ).html(html);
+        $(document).click(function(event) {
+            if(!$(event.target).closest('.groupSelect').length) {
+                $(".options").addClass('group-select-hide');
+            }else{
+                if($(event.target).closest('.select').length) {
+                    if ($(".options").hasClass('group-select-hide')) {
+                        $(".options").removeClass('group-select-hide');
+                    } else {
+                        $(".options").addClass('group-select-hide');
+                    }
+                }else{
+                    $(".options").removeClass('group-select-hide');
+                }
+            }
+        });
+        $(".group-select-option").on('click', function(){
+            var valor = $(this).val();
+            var parent = valor.toLowerCase().replace(/ /g,'-');
+            $('.'+parent).css('display', 'block');
+            $('.subitem-group').each(function () {
+                if(!$( this ).hasClass(parent)) {
+                    $(this).css('display', 'none');
+                }
+            });
+            $('.select').html('<div class="group-selection"><div class="parent">'+valor+'</div><div class="children"></div></div>')
+            $('.options').find('[data-parent="'+parent+'"]').each(function () {
+               $(this).prop('checked', false);
+            });
+            settings.onChange(valor, []);
+        });
+
+        $(".group-select-option-subitem").on('change', function(){
+            var parent = $(this).data('parent')
+            var parent_label = $('.select').find('.group-selection').find(".parent").text()
+            var children = []
+            $('.'+parent).find("[data-parent='"+parent+"']").each(function () {
+                if($(this).is(':checked') == true){
+                    children.push($(this).val());
+                }
+            });
+            var selected = "&nbsp;:"
+            visible_children = $.each(children, function () {
+                selected += "<div class='subitem-label'>"+this+"hhh</div>";
+            });
+
+            $('.select').find('.group-selection').find('.children').html(selected);
+            settings.onChange($('.select').find('.group-selection').find('.parent').text(), children);
+        });
+
+        return this
+
+    };
+})(jQuery);
